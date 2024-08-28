0d9692f0f5abedaddb0e36e29cc59c2150af02ff
Ivan Escobar <ivnescbr1989@gmail.com>
2024-08-28 16:35:58 -0400

    learning stuffs

---
+++ CsharpProjects/Excercise/Program.cs
@@ -0,0 +1,35 @@
+﻿Random random = new();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+// Your code goes here
+Console.WriteLine($"Days till expiration {daysUntilExpiration}");
+if (daysUntilExpiration <= 5)
+{
+    discountPercentage = 10;
+}
+else
+{
+    discountPercentage = 20;
+}
+
+if (daysUntilExpiration <= 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\nRenew now and save {discountPercentage}%!");
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\nRenew now and save {discountPercentage}%!");
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+else
+{
+    Console.WriteLine("");
+}

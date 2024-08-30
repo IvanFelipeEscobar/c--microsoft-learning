67ade0597d5333bda205094f7b8faf10cb6c3c82
Ivan Escobar <ivnescbr1989@gmail.com>
2024-08-30 11:56:49 -0400

    evaluating conditional expressions module

---
+++ CsharpProjects/conditional-excercise/Program.cs
@@ -0,0 +1,24 @@
+﻿// Random coin = new();
+// int coinFlip = coin.Next(10);
+// Console.WriteLine(coinFlip < 5 ? "heads" : "tails" );
+
+string permission = "Admin|Manager";
+int level = 55;
+
+if (permission.Contains("Admin"))
+{
+    Console.WriteLine(level > 55 ? "Welcome, Super Admin user." : "Welcome, Admin user.");
+}
+
+else if (permission.Contains("Manager") && level >= 20)
+{
+    Console.WriteLine("Contact an Admin for access.");
+}
+
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
+
+
+
---
+++ CsharpProjects/conditional-logic/Program.cs
@@ -0,0 +1,48 @@
+﻿
+Console.Clear();
+
+Console.Write("\"a\" == \"a\"\t");
+Console.WriteLine("a" == "a");
+
+Console.Write("\"a\" == \"A\"\t");
+Console.WriteLine("a" == "A");
+
+Console.Write("1 == 2\t");
+Console.WriteLine(1 == 2);
+Console.WriteLine("----------------------------------------------------------- ");
+string myValue = "a";
+Console.WriteLine(myValue == "a");
+Console.WriteLine("----------------------------------------------------------- ");
+
+string value1 = " a";
+string value2 = "A ";
+Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+Console.WriteLine("----------------------------------------------------------- ");
+
+Console.WriteLine("a" != "a");
+Console.WriteLine("a" != "A");
+Console.WriteLine(1 != 2);
+string myValue1 = "a";
+Console.WriteLine(myValue1 != "a");
+Console.WriteLine("----------------------------------------------------------- ");
+
+Console.WriteLine(1 > 2);
+Console.WriteLine(1 < 2);
+Console.WriteLine(1 >= 1);
+Console.WriteLine(1 <= 1);
+
+Console.WriteLine("----------------------------------------------------------- ");
+
+string pangram = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(pangram.Contains("fox"));
+Console.WriteLine(pangram.Contains("cow"));
+
+Console.WriteLine("----------------------------------------------------------- ");
+
+string pangram1 = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(!pangram1.Contains("fox"));
+Console.WriteLine(!pangram1.Contains("cow"));
+
+Console.WriteLine("----------------------------------------------------------- ");
+int saleAmount = 1001;
+Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");

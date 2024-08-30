3340c7f2a9c2f5d2b72ee64b15c8ca5bd634695e
Ivan Escobar <ivnescbr1989@gmail.com>
2024-08-30 17:15:21 -0400

    iteration module/fizzbuzz echallenge

---
+++ CsharpProjects/fizz-buzz-challenge/Program.cs
@@ -0,0 +1,12 @@
+﻿for (int i = 1; i < 101; i++)
+{
+    if (i % 5 == 0 && i % 3 == 0)
+        Console.WriteLine($"{i}\t-\tFizzBuzz");
+    else if (i % 5 == 0)
+        Console.WriteLine($"{i}\t-\tBuzz");
+    else if (i % 3 == 0)
+        Console.WriteLine($"{i}\t-\tFizz");
+    else
+        Console.WriteLine(i);
+
+}
---
+++ CsharpProjects/iteration-excercises/Program.cs
@@ -0,0 +1,30 @@
+﻿// for (int i = 0; i < 10; i +=2)
+// {
+//     Console.WriteLine(i);
+// }
+// for (int i = 10; i >= 0; i--)    
+// {
+//     Console.WriteLine(i);
+// }
+// for (int i = 0; i < 10; i++)
+// {
+//     Console.WriteLine(i);
+//     if (i == 7) break;
+// }
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+for (int i = names.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(names[i]);
+}
+
+Console.WriteLine("\nchange here\t\thit enter to continue\n");
+Console.Read();
+
+string[] otherNames = { "Alex", "Eddie", "David", "Michael" };
+for (int i = 0; i < otherNames.Length; i++)
+    if (otherNames[i] == "David") otherNames[i] = "Sammy";
+
+foreach (var name in otherNames) Console.WriteLine(name);
+
+Console.WriteLine("\n\nhit enter to continue");
+Console.Read();
